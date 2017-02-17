8f30bc0e1e8f1f84c36c97ae95a6881880d249c1
imthefrizzlefry <imthefrizzlefry@gmail.com>
2017-02-17 12:57:03 -0800

    switched implementation to prototype method

--- src/stringFormat.js
+++ src/stringFormat.js
@@ -1,4 +1,22 @@
 'use strict'
+/*
+a long story of Google searches on this one.  The best solution is the String.prototype.format function.   
+I did have some confusion as to why I was unable to convert this method to work with my own method, 
+which I will look into, but take a look below to find some of the other ideas I found online.
+I took this solution from:
+  http://stackoverflow.com/questions/1038746/equivalent-of-string-format-in-jquery
+
+  Then I modified the return to check for undefined, and handle that gradefully.
+  (see line 17)
+*/
+String.prototype.format = function () {
+  var args = arguments;
+  return this.replace(/\{\{|\}\}|\{(\d+)\}/g, function (m, n) {
+    if (m == "{{") { return "{"; }
+    if (m == "}}") { return "}"; }
+    return (typeof args[n] != 'undefined') ? args[n] : m;
+  });
+};
 
 var stringFormat = function() {
     var format = arguments[0],
@@ -19,8 +37,37 @@ var stringFormat = function() {
             output = format;
         }
     }
-
     return output;
 }
+
+    // I found this loop on some guys blog:
+    //https://cyanbyfuchsia.wordpress.com/2012/06/23/string-format-in-javascript/#comment-2002
+
+    // The problem is that it fails the cases where the formatted string contains {#} where {#} is the index of a valid parameter 
+    // for (var i = 1; i < arguments.length; i++) {
+    //     var regEx = new RegExp("\\{" + i + "\\}", "gm");
+    //     s = s.replace(regEx, arguments[i]);
+    // }
+
+    // when I first found the prototype example, I wanted to someone put that into this function
+    // var temp = arguments[0].replace(/\{(\d+)\}/g, function (m, n) { return arguments[n]; });
+    // console.log(arguments[0]);
+    // return temp;
+
+    // var s = arguments[0];    
+    // s = s.replace(/\{(\d+)\}/g, function (m, n) { return arguments[n]; });
+    // return s;
+
+
+
+// String.format = function () {
+//     var s = arguments[0];
+//     for (var i = 0; i < arguments.length - 1; i++) {
+//         var reg = new RegExp("\\{" + i + "\\}", "gm");
+//         s = s.replace(reg, arguments[i + 1]);
+//     }
+//     return s;
+// }
+
 
 module.exports.stringFormat = stringFormat;
--- test/stringFormatTests.js
+++ test/stringFormatTests.js
@@ -1,61 +1,61 @@
 var assert = require("assert");
 var chai = require('chai');
-var StringFormat = require('../src/stringFormat');
+var StringFormat = require('../src/stringFormat.js');
 chai.should();
 
 describe("The String Format method", function(){
 
   it("Should be able to combine strings together", function(){
     var expectedString = "abc";
-    var actualString = StringFormat.stringFormat("{1}{2}{3}", "a", "b", "c")
+    var actualString = "{0}{1}{2}".format("a", "b", "c"); // StringFormat.stringFormat("{1}{2}{3}", "a", "b", "c")
     actualString.should.equal(expectedString);
   });
 
   it("Should be able to skip arguements that are not used in format", function(){
     var expectedString = "abc";
-    var actualString = StringFormat.stringFormat("ab{3}", "x", "y", "c")
+    var actualString = "ab{2}".format("x", "y", "c"); // StringFormat.stringFormat("ab{3}", "x", "y", "c")
     actualString.should.equal(expectedString);
   });
 
   it("Should be able to ignore format that includes a reference to non-existant parameters", function(){
     var expectedString = "ab{3}";
-    var actualString = StringFormat.stringFormat("ab{3}", "x", "y")
+    var actualString = "ab{3}".format("x", "y"); // StringFormat.stringFormat("ab{3}", "x", "y")
     actualString.should.equal(expectedString);
   });
 
   it("Should not replace values based on formatted result", function(){
     var expectedString = "a{3}b";
-    var actualString = StringFormat.stringFormat("{1}3{2}", "a{", "}b", "c")
+    var actualString = "{0}3{1}".format("a{", "}b", "c"); // StringFormat.stringFormat("{1}3{2}", "a{", "}b", "c")
     actualString.should.equal(expectedString);
   });
 
   it("Should not replace values that do not need replacing", function(){
     var expectedString = "nothing to see here";
-    var actualString = StringFormat.stringFormat("nothing to see here", "a", "b", "c")
+    var actualString = "nothing to see here".format("a", "b", "c"); // StringFormat.stringFormat("nothing to see here", "a", "b", "c")
     actualString.should.equal(expectedString);
   });
 
   it("Should be able to handle multiple character replacements in format", function(){
     var expectedString = "We Love Tests";
-    var actualString = StringFormat.stringFormat("{1} {2} {3}", "We", "Love", "Tests")
+    var actualString = "{0} {1} {2}".format("We", "Love", "Tests"); // StringFormat.stringFormat("{1} {2} {3}", "We", "Love", "Tests")
     actualString.should.equal(expectedString);
   });
 
   it("Should be able to handle multiple character replacements in format with special characters", function(){
-    var expectedString = "We Lo{3}ve Tests";
-    var actualString = StringFormat.stringFormat("{1} {2} {3}", "We", "Lo{3}ve", "Tests")
+    var expectedString = "We Lo{2}ve Tests";
+    var actualString = "{0} {1} {2}".format("We", "Lo{2}ve", "Tests"); // StringFormat.stringFormat("{1} {2} {3}", "We", "Lo{3}ve", "Tests")
     actualString.should.equal(expectedString);
   });
 
   it("Should be able to replace an attribute that appears multiple times in format", function(){
     var expectedString = "Foo Bar: Foo Baz";
-    var actualString = StringFormat.stringFormat("{1} {2}: {1} {3}", "Foo", "Bar", "Baz")
+    var actualString = "{0} {1}: {0} {2}".format("Foo", "Bar", "Baz"); // StringFormat.stringFormat("{1} {2}: {1} {3}", "Foo", "Bar", "Baz")
     actualString.should.equal(expectedString);
   });
 
   it("Should be able to replace an attribute that is not in order within format", function(){
     var expectedString = "Bar Foo: Foo Baz";
-    var actualString = StringFormat.stringFormat("{2} {1}: {1} {3}", "Foo", "Bar", "Baz")
+    var actualString = "{1} {0}: {0} {2}".format("Foo", "Bar", "Baz"); // StringFormat.stringFormat("{2} {1}: {1} {3}", "Foo", "Bar", "Baz")
     actualString.should.equal(expectedString);
   });    
 
