cad729d2dfe55f87e2446924f33914c2238b032b
imthefrizzlefry <imthefrizzlefry@gmail.com>
2017-02-16 19:18:47 -0800

    adjusting for readability

--- HBO/src/canonicalPath.js
+++ HBO/src/canonicalPath.js
@@ -3,31 +3,44 @@ var canonicalPath = {
     isPlatformWindows: function() { return process.platform == 'win32'},
     getUserDirectory: function(runningWindows) {return process.env[runningWindows ? 'USERPROFILE' : 'HOME']; },
     getCurrentDirectory: function() {return __dirname},
-    Get: function() {
-
+    Get: function(relativePath) {
+        //declaring new variables for method
         var runningOnWindows = this.isPlatformWindows()
         var dirDelimiter = runningOnWindows ? "\\" : "/";
-        var relativePath = arguments[0].split(dirDelimiter);
-        var base = this.getCurrentDirectory();
+        var base = "";
         var i = 0;
+
+        // handling home directory exception
         if(relativePath[i] == "~") {
             base = this.getUserDirectory(runningOnWindows);
             i++;
+        } else {
+            base = this.getCurrentDirectory();
         }
 
+        // splitting paths into arrays to make them easier to work with
+        relativePath = relativePath.split(dirDelimiter);
         base = base.split(dirDelimiter);
 
+        // loop through entries in relative path, and process each part
         for(i; i < relativePath.length; i++) {
+            // ignore dot because it's the current directory
             if(relativePath[i] == "."){
                 continue;
             }
+
+            // either append directory name or pop for parent directory
             if(relativePath[i] == "..") {
                 base.pop();
             } else {
                 base.push(relativePath[i]);
             }
         }
+
+        // recombine path into a single string delimited by the appropriate path delimiter
         var output = base.join(dirDelimiter)
+
+        // strip trailing delimiter for the case of ./ and ~/
         return (output.slice(-1) == dirDelimiter) ? output.slice(0,-1) : output;
     } 
 
