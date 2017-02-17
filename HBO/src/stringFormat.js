3565aeb000fb8b93dc0a1c56b5b9e164f3fe99ac
imthefrizzlefry <imthefrizzlefry@gmail.com>
2017-02-17 00:56:49 -0800

    Adding tests for stringFormat.  I broke it

--- HBO/src/stringFormat.js
+++ HBO/src/stringFormat.js
@@ -1,12 +1,23 @@
 'use strict'
 
 var stringFormat = function() {
-    var output = arguments[0];
-    var replacements = arguments.length;
+    var format = arguments[0],
+        replacements = arguments.length,
+        output = "",
+        startIndex = 0,
+        endIndex = 0;
 
     for(var i=1; i < replacements; i++) {
         var replacement = "{" + i + "}";
-        output = output.replace(replacement, arguments[i]);
+        if(format.includes(replacement))
+        {
+            endIndex = startIndex + format.slice(startIndex).indexOf(replacement) + 3;
+            output += format.substring(startIndex, endIndex).replace(replacement, arguments[i]);
+            startIndex = endIndex;
+        } else if (i+1 == replacements & startIndex == 0)
+        {
+            output = format;
+        }
     }
 
     return output;
--- HBO/test/stringFormatTests.js
+++ HBO/test/stringFormatTests.js
@@ -5,10 +5,58 @@ chai.should();
 
 describe("The String Format method", function(){
 
-  it("Should be albe to replace a single ", function(){
+  it("Should be able to combine strings together", function(){
     var expectedString = "abc";
     var actualString = StringFormat.stringFormat("{1}{2}{3}", "a", "b", "c")
     actualString.should.equal(expectedString);
   });
+
+  it("Should be able to skip arguements that are not used in format", function(){
+    var expectedString = "abc";
+    var actualString = StringFormat.stringFormat("ab{3}", "x", "y", "c")
+    actualString.should.equal(expectedString);
+  });
+
+  it("Should be able to ignore format that includes a reference to non-existant parameters", function(){
+    var expectedString = "ab{3}";
+    var actualString = StringFormat.stringFormat("ab{3}", "x", "y")
+    actualString.should.equal(expectedString);
+  });
+
+  it("Should not replace values based on formatted result", function(){
+    var expectedString = "a{3}b";
+    var actualString = StringFormat.stringFormat("{1}3{2}", "a{", "}b", "c")
+    actualString.should.equal(expectedString);
+  });
+
+  it("Should not replace values that do not need replacing", function(){
+    var expectedString = "nothing to see here";
+    var actualString = StringFormat.stringFormat("nothing to see here", "a", "b", "c")
+    actualString.should.equal(expectedString);
+  });
+
+  it("Should be able to handle multiple character replacements in format", function(){
+    var expectedString = "We Love Tests";
+    var actualString = StringFormat.stringFormat("{1} {2} {3}", "We", "Love", "Tests")
+    actualString.should.equal(expectedString);
+  });
+
+  it("Should be able to handle multiple character replacements in format with special characters", function(){
+    var expectedString = "We Lo{3}ve Tests";
+    var actualString = StringFormat.stringFormat("{1} {2} {3}", "We", "Lo{3}ve", "Tests")
+    actualString.should.equal(expectedString);
+  });
+
+  it("Should be able to replace an attribute that appears multiple times in format", function(){
+    var expectedString = "Foo Bar: Foo Baz";
+    var actualString = StringFormat.stringFormat("{1} {2}: {1} {3}", "Foo", "Bar", "Baz")
+    actualString.should.equal(expectedString);
+  });
+
+  it("Should be able to replace an attribute that is not in order within format", function(){
+    var expectedString = "Bar Foo: Foo Baz";
+    var actualString = StringFormat.stringFormat("{2} {1}: {1} {3}", "Foo", "Bar", "Baz")
+    actualString.should.equal(expectedString);
+  });    
 
 });
