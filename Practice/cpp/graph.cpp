9f9735321a2d20a850552e258bd910d6a21f8c47
Steven Farnell <a-sfarnell@expedia.com>
2017-09-21 14:49:49 -0700

    updating gcc command

--- Practice/cpp/graph.cpp
+++ Practice/cpp/graph.cpp
@@ -1,3 +1,4 @@
+// compile and run: gcc ./graph.cpp -o graph -lstdc++ ; ./graph
 // Program to print BFS traversal from a given source vertex. BFS(int s) 
 // traverses vertices reachable from s.
 #include <iostream>
