940180100082faa9f27b893e04de38a8910585a1
imthefrizzlefry <imthefrizzlefry@gmail.com>
2018-09-15 13:39:40 -0700

    small fixes

--- Practice/python/banking.py
+++ Practice/python/banking.py
@@ -1,6 +1,6 @@
 
 
-def calcSteppedInterestRate(self, balance, lowerRate=.0617, upperRate=.001, divider=500):
+def calcSteppedInterestRate(balance, lowerRate=.0617, upperRate=.001, divider=500):
 
     if balance > divider:
         return (divider * lowerRate) + ((balance - divider) * upperRate)
@@ -8,17 +8,12 @@ def calcSteppedInterestRate(self, balance, lowerRate=.0617, upperRate=.001, divi
         return balance * lowerRate
 
 if __name__ == '__main__':
-    # do by default
-    x = RetirementCalc(age=35, income= 200181.18, balance= 123435.12, contributionRate=0.1421, growthRate=0.06)
-    #print(x.calcProjectedRetirementAge())
-
-    #print(x.calcContributionAmountByRetirementAge())
-
+    annualDeposit = 100
     balance = 100
     print(balance)
     for age in range(2,18):
-        interest = x.calcSteppedInterestRate(balance) 
+        interest = calcSteppedInterestRate(balance) 
         balance += interest
-        print('the Effective Interest Rate will be {0:.2f}, and new balance is {1:.2f}'.format(interest/balance, balance))
-        balance += 100
-        print('new deposit of 100 bring account to {0:.2f}'.format(balance))
+        print('the Effective Interest Rate will be {0:.3f}, and new balance is {1:.2f}'.format(interest/balance, balance))
+        balance += annualDeposit
+        print('new deposit of {0} bring account to {1:.2f}'.format(annualDeposit, balance))
